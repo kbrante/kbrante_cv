70e713b89e01bfec0f817c7243b52dafaa798ed7
karina BRANTE <karina.brante@gmail.com>
2017-07-28 15:42:31 +0200

    configuration django requirements

--- karina/settings.py
+++ karina/settings.py
@@ -31,12 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_extensions',
+    'djangobower',
+    'sass_processor',
+    'cv',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,8 @@ ROOT_URLCONF = 'karina.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'karina', 'templates')
+],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +111,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CET'
 
 USE_I18N = True
 
@@ -118,3 +126,81 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+
+STATICFILES_FINDERS = [
+
+   'django.contrib.staticfiles.finders.FileSystemFinder',
+
+   'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+
+   'djangobower.finders.BowerFinder',
+
+   'sass_processor.finders.CssFinder',
+
+]
+
+BOWER_INSTALLED_APPS = (
+
+   'jquery',
+
+   'bootstrap-sass',
+
+   'fontawesome',
+
+   'tagsinput',
+
+)
+
+SASS_PROCESSOR_INCLUDE_DIRS = [
+
+   os.path.join(BASE_DIR, 'static/bower_components/bootstrap-sass/assets/stylesheets'),
+
+]
+
+BOWER_COMPONENTS_ROOT = os.path.join(BASE_DIR, "static")
+
+GRAPPELLI_ADMIN_TITLE = "Social Network Django"
+
+AUTHENTICATION_BACKENDS = (
+
+
+   'django.contrib.auth.backends.ModelBackend',
+   'allauth.account.auth_backends.AuthenticationBackend'
+
+)
+
+SITE_ID = 1
+
+# ACCOUNT_EMAIL_REQUIRED = True
+# ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
+
+TAGGIT_CASE_INSENSITIVE = True
+MEDIA_ROOT = os.path.join(BASE_DIR, "people", "static", "images")
+MEDIA_URL = '/static/images/'
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR,"locale")
+]
+
+PARLER_DEFAULT_LANGUAGE_CODE = 'fr'
+
+PARLER_LANGUAGES = {
+    1: (
+        {'code': 'en',},
+        {'code': 'es',},
+        {'code': 'fr',},
+    ),
+    'default': {
+        'fallbacks': ['en'],          # defaults to PARLER_DEFAULT_LANGUAGE_CODE
+        'hide_untranslated': False,   # the default; let .active_translations() return fallbacks too.
+    }
+}
